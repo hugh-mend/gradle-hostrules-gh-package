a0e9e8bfa8d5384fd5c8c14958ac1332e82f3b2d
Hugh Cushing <hugh.cushing@whitesourcesoftware.com>
2024-09-14 21:16:39 -0400

    Getting 2 deps from two different GH package repos

---
+++ devops-github-packages-library3/src/main/java/io/flowsquad/blog/devops/github/packages/library2/LibraryClass.java
@@ -0,0 +1,7 @@
+package io.flowsquad.blog.devops.github.packages.library2;
+
+public class LibraryClass {
+    public String utilityMethod() {
+        return "Hi, I'm a second very expensive library method and do a lot of heavy calculations!";
+    }
+}
