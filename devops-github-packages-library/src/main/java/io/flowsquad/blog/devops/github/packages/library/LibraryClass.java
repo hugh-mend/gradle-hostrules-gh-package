c0f6caf89a36db2e27b50a0814c2dd1223724cda
Hugh Cushing <hugh.cushing@whitesourcesoftware.com>
2024-09-17 23:06:10 -0400

    Apply greeting plugin

--- devops-github-packages-library/src/main/java/io/flowsquad/blog/devops/github/packages/library/LibraryClass.java
+++ devops-github-packages-library/src/main/java/io/flowsquad/blog/devops/github/packages/library/LibraryClass.java
@@ -2,6 +2,6 @@ package io.flowsquad.blog.devops.github.packages.library;
 
 public class LibraryClass {
     public String utilityMethod() {
-        return "Hi, I'm a very very expensive library method and do a lot of heavy calculations!";
+        return "Hi, I'm a very expensive library method and do a lot of heavy calculations!";
     }
 }
