29902f867b171e66dfe39de7c31ec8d31d97a611
Hugh Cushing <hugh.cushing@whitesourcesoftware.com>
2024-09-20 11:32:40 -0400

    Down to one class call

--- devops-github-packages-main/src/main/java/io/flowsquad/blog/devops/github/packages/main/MainClass.java
+++ devops-github-packages-main/src/main/java/io/flowsquad/blog/devops/github/packages/main/MainClass.java
@@ -1,12 +1,7 @@
 package io.flowsquad.blog.devops.github.packages.main;
 
 import io.flowsquad.blog.devops.github.packages.library.LibraryClass;
-import io.flowsquad.blog.devops.github.packages.library2.LibraryClass;
-import io.flowsquad.blog.devops.github.packages.library3.LibraryClass;
 
 public class MainClass {
-    public static void main(String[] args) {
-        System.out.println(new LibraryClass().utilityMethod());
-    }
     public static void main(String[] args) { System.out.println(new LibraryClass().utilityMethod()); }
 }
