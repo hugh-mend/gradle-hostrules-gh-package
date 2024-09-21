b82625be23745d79c44aa1195e7c1c5723b1956a
Hugh Cushing <hugh.cushing@whitesourcesoftware.com>
2024-09-20 22:02:00 -0400

    Down to one class call

--- devops-github-packages-library2/src/main/java/io/flowsquad/blog/devops/github/packages/library2/LibraryClass2.java
+++
@@ -1,7 +0,0 @@
-package io.flowsquad.blog.devops.github.packages.library2;
-
-public class LibraryClass2 {
-    public String utilityMethod() {
-        return "Hi, I'm a second very expensive library method and do a lot of heavy calculations!";
-    }
-}
